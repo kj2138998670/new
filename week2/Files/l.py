9b181ca93e62161d72e8f8eaa2d8344aaf173b73
kj2138998670 <haonan.wang@my.jcu.edu.au>
2019-01-15 23:02:52 +0800

    Assignment1

--- week2/Files/l.py
+++
@@ -1,4 +0,0 @@
-out_file = open("name.txt", "w")
-name = input("What is your name? ")
-print(name, file=out_file) # or outFile.write(name)
-out_file.close()
--- week2/Files/n.py
+++
@@ -1,5 +0,0 @@
-in_file = open("numbers.txt","r")
-number1=int(in_file.readline())
-number2=int(in_file.readline())
-print((number1+number2))
-in_file.close()
